81d06f8ad4e03b4347b11f38ed999ccfba8b005a
Gino Torres <gino.torresjorquera13.99@gmail.com>
2022-06-25 20:16:59 -0400

    avance calculadora

--- source/proyecto1/yonex/calculadora/src/App.js
+++ source/proyecto1/yonex/calculadora/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import theEndlessRiverLogo from './img/theEndlessRiver.jpg';
+import Boton from './componentes/Boton';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'> 
+      <div className='theEndlessRiver-logo-contenedor' >
+        <img
+        src={theEndlessRiverLogo}
+        className='theEndlessRiver-logo'
+        alt='theEndlessRiver'
+         />
+        <div className='contenedor-calculadora'>
+          <div className='fila'>
+            <Boton>1</Boton>
+            <Boton>2</Boton>
+            <Boton>3</Boton>
+            <Boton>+</Boton>
+          </div>
+          <div className='fila'>
+            <Boton>4</Boton>
+            <Boton>5</Boton>
+            <Boton>6</Boton>
+            <Boton>-</Boton>
+          </div>
+          <div className='fila'>
+            <Boton>7</Boton>
+            <Boton>8</Boton>
+            <Boton>9</Boton>
+            <Boton>*</Boton>
+          </div>
+          <div className='fila'>
+            <Boton>=</Boton>
+            <Boton>0</Boton>
+            <Boton>.</Boton>
+            <Boton>/</Boton>
+          </div>
+          <div className='fila'>
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
