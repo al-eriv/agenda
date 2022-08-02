ccb2386a59fc6a18759b77488b1b6d2f2e37d19c
cocolisoft <arivera.esl@gmail.com>
2022-08-01 20:40:26 -0400

    muchos cambios

---
+++ source/proyecto3/alexis/calculadora/src/App.js
@@ -0,0 +1,66 @@
+import './App.css';
+import calculadoraLogo from './imagenes/calculadora-logo.png'
+import Boton from './componentes/boton'
+import Pantalla from './componentes/Pantalla';
+import BotonClear from './componentes/BotonClear';
+import { useState } from 'react';
+import { evaluate } from 'mathjs';
+
+function App() {
+
+  const [input, setInput] = useState('');
+
+  const agregarInput = val => {
+    setInput(input + val);
+  };
+
+  const calcularResultado = () => {
+    setInput(evaluate(input));
+  }
+
+  return (
+    <div className="App">
+      <div className='logo-contenedor'>
+        <img
+          src={calculadoraLogo}
+          className='calculadora-logo'
+          alt='Logo Calculadora' />
+
+      </div>
+      <div className='contenedor-calculadora'>
+        <Pantalla input={input} />
+        <div className='fila'>
+          <Boton manejarClick={agregarInput} >1</Boton>
+          <Boton manejarClick={agregarInput}>2</Boton>
+          <Boton manejarClick={agregarInput}>3</Boton>
+          <Boton manejarClick={agregarInput}>+</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClick={agregarInput}>4</Boton>
+          <Boton manejarClick={agregarInput}>5</Boton>
+          <Boton manejarClick={agregarInput}>6</Boton>
+          <Boton manejarClick={agregarInput}>-</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClick={agregarInput}>7</Boton>
+          <Boton manejarClick={agregarInput}>8</Boton>
+          <Boton manejarClick={agregarInput}>9</Boton>
+          <Boton manejarClick={agregarInput}>*</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClick={calcularResultado}>=</Boton>
+          <Boton manejarClick={agregarInput}>0</Boton>
+          <Boton manejarClick={agregarInput}>.</Boton>
+          <Boton manejarClick={agregarInput}>/</Boton></div>
+          
+        <div className='fila'>
+          <BotonClear manejarClear={() => setInput('')}>
+            Clear
+          </BotonClear>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
